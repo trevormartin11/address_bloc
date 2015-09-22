e7f6cb3551e5433b3dd1b49d73f6f37217d84476
Trevor <trevormartin11@gmail.com>
2015-09-21 18:36:12 -0700

    fixed file structure

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -13,7 +13,8 @@ class MenuController
     puts "2 - Create an entry"
     puts "3 - Search for an entry"
     puts "4 - Import entries from a CSV"
-    puts "5 - Exit"
+    puts "5 - View entry number n"
+    puts "6 - Exit"
     print "Enter you selection: "
 
     selection = gets.to_i
@@ -35,6 +36,10 @@ class MenuController
       read_csv
       main_menu
     when 5
+      system "clear"
+      entry_n_submenu
+      main_menu
+    when 6
       puts "Good-bye!"
       exit(0)
     else
@@ -43,6 +48,22 @@ class MenuController
       main_menu
     end
   end
+
+  def entry_n_submenu
+    print "Entry number to view: "
+    selection = gets.chomp.to_i
+
+    if selection <= @address_book.entries.count
+      puts @address_book.entries[selection]
+      puts "Press enter to return to the main menu"
+      gets.chomp
+      system "clear"
+    else
+      puts "#{selection} is not a valid input"
+      entry_n_submenu
+    end
+  end
+
 
   def view_all_entries
     @address_book.entries.each do |entry|
@@ -100,3 +121,5 @@ class MenuController
     end
   end
 end
+
+
